268a857ff0158e5ed17ea3efd26cd07c46ec18e0
Benson Njuguna <njugunab655@gmail.com>
2023-05-01 11:42:53 +0300

    Add header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>todos</h1>
+      <p>List of items</p>
+    </div>
+  )
+}
+
+export default Header;
